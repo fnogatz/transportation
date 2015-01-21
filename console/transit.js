31769dc058661c6d5b61d0cb668280f90ca000a4
Falco Nogatz <fnogatz@gmail.com>
2015-01-21 21:55:33 +0100

    Add "# Shapes" to console/transit

--- console/transit.js
+++ console/transit.js
@@ -6,11 +6,13 @@ module.exports = {
     'Agencies': function() { return this.agencies.ids.join('\n'); },
     '# Stops': function() { return this.stops.length; },
     '# Services': function() { return this.services.length; },
+    '# Shapes': function() { return this.shapes.length; },
   },
   defaultFields: [
     'Agencies',
     '# Stops',
     '# Services',
+    '# Shapes',
   ],
   test: function() {
     return this instanceof Transit;
