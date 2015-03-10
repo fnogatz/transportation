068db71879ef5b3893ca5e8b213f32bc1e289e24
Falco Nogatz <fnogatz@gmail.com>
2015-03-10 21:44:22 +0100

    Combine Trip.stops and Trip.shape into Trip.positions

--- console/positions.js
+++ console/positions.js
@@ -3,18 +3,23 @@ module.exports = {
     '#': function(i) { return i+1; },
     'Lon': function() { return this.lon; },
     'Lat': function() { return this.lat; },
+    'Time': function() { return this.time; },
     'Distance': function() { return this.distance; },
+    'Travel Distance': function() { return this.travelDistance; },
   },
   defaultFields: [
     'Lon',
     'Lat',
+    'Time',
+    'Distance',
+    'Travel Distance',
   ],
   test: function() {
     return (typeof this === 'object' &&
         this instanceof Array &&
         allWithProperty(this, 'lon') &&
         allWithProperty(this, 'lat') &&
-        !allWithProperty(this, 'time'));
+        allWithProperty(this, 'time'));
   },
 };
 
