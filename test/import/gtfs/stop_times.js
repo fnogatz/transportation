1306ee11d5b7ec9d5e48289e210de027d35757b1
Falco Nogatz <fnogatz@gmail.com>
2015-09-07 14:10:28 +0200

    Use JavaScript Standard Style; Add test for malformed HHMMSS time in stop_times

--- test/import/gtfs/stop_times.js
+++ test/import/gtfs/stop_times.js
@@ -8,6 +8,73 @@ var Stop = require('../../../lib/stop')
 var importStopTimes = require('../../../lib/import/gtfs/import.stop_times')
 
 test('import stop_times', function (t) {
+  var transit = initTransit()
+
+  t.test('pickup time', function (t) {
+    importStopTimes(__dirname + '/data/pickup-dropoff-types/stop_times.txt', transit, function onEnd () {
+      var stopTimes = transit.agencies.DTA.routes.STBA.trips.STBA.stops
+      t.equal(stopTimes.length, 3)
+
+      t.similar(stopTimes[1], {
+        id: 1,
+        arrival: '06:00:00',
+        departure: '06:00:00',
+        pickupType: 0,
+        dropoffType: 0
+      })
+
+      t.similar(stopTimes[2], {
+        id: 2,
+        arrival: '06:20:00',
+        departure: '06:20:00',
+        pickupType: 0,
+        dropoffType: 1
+      })
+
+      t.similar(stopTimes[3], {
+        id: 3,
+        arrival: '06:40:00',
+        departure: '06:40:00',
+        pickupType: 1,
+        dropoffType: 0
+      })
+
+      t.end()
+    })
+  })
+
+  t.end()
+})
+
+test('warning on malformed stop times', function (t) {
+  var transit = initTransit()
+
+  var warnings = []
+
+  // catch console.warn calls
+  console.warn = function (msg) {
+    warnings.push(msg)
+  }
+
+  importStopTimes(__dirname + '/data/malformed-stop-times/stop_times.txt', transit, function onEnd () {
+    var stopTimes = transit.agencies.DTA.routes.STBA.trips.STBA.stops
+    t.equal(stopTimes.length, 1)
+
+    t.similar(stopTimes[1], {
+      id: 1,
+      arrival: '06:00:00',
+      departure: '06:00:00',
+      pickupType: 0,
+      dropoffType: 0
+    })
+
+    t.equal(warnings.length, 2, 'two times not valid')
+
+    t.end()
+  })
+})
+
+function initTransit () {
   var transit = new Transit()
 
   transit.agencies.add(new Agency({
@@ -21,41 +88,18 @@ test('import stop_times', function (t) {
 
   transit.route('STBA').trips.add(new Trip({id: 'STBA'}), 'STBA')
 
-  transit.stops.add(new Stop({
-    id: 'STAGECOACH',
-    name: 'Stagecoach Hotel & Casino (Demo)',
-    lat: 36.915682,
-    lon: -116.751677
-  }))
-
-  importStopTimes(__dirname + '/data/pickup-dropoff-types/stop_times.txt', transit, function onEnd () {
-    var stopTimes = transit.agencies.DTA.routes.STBA.trips.STBA.stops
-    t.equal(stopTimes.length, 3)
-
-    t.similar(stopTimes[1], {
-      id: 1,
-      arrival: '06:00:00',
-      departure: '06:00:00',
-      pickupType: 0,
-      dropoffType: 0
-    })
-
-    t.similar(stopTimes[2], {
-      id: 2,
-      arrival: '06:20:00',
-      departure: '06:20:00',
-      pickupType: 0,
-      dropoffType: 1
-    })
-
-    t.similar(stopTimes[3], {
-      id: 3,
-      arrival: '06:40:00',
-      departure: '06:40:00',
-      pickupType: 1,
-      dropoffType: 0
-    })
-
-    t.end()
+  ;[
+    'STAGECOACH',
+    'BEATTY_AIRPORT',
+    'EMSI'
+  ].forEach(function (code) {
+    transit.stops.add(new Stop({
+      id: code,
+      name: '(Demo)',
+      lat: 36.915682,
+      lon: -116.751677
+    }))
   })
-})
+
+  return transit
+}
