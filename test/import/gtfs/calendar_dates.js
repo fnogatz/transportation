2cd5d54c8f22ac2fb8a59a921492df2e69938d2c
Falco Nogatz <fnogatz@gmail.com>
2019-08-26 21:46:28 +0200

    Use standard@14.0.2

--- test/import/gtfs/calendar_dates.js
+++ test/import/gtfs/calendar_dates.js
@@ -11,8 +11,8 @@ test('import calendar dates', function (t) {
   importCalendarDates(path.resolve(__dirname, 'data/generic/calendar_dates.txt'), transit, function onEnd () {
     t.equal(transit.services.length, 1)
 
-    t.ok(transit.services['FULLW'])
-    t.equal(transit.services['FULLW'].exceptions['20070604'], Service.Exception.Removed)
+    t.ok(transit.services.FULLW)
+    t.equal(transit.services.FULLW.exceptions['20070604'], Service.Exception.Removed)
 
     t.end()
   })
